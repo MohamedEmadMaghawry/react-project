1937726a18abf4ab48cfcb8c51b3e69cbc22f69a
MohamedEmadMaghawry <146999123+MohamedEmadMaghawry@users.noreply.github.com>
2024-04-04 22:47:49 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from './pages/home';
+import Html from './pages/html';
+import Css from './pages/css';
+import Javascript from './pages/javascript';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>Page does not Exist</h1>
+  },
+  {
+    path: "html",
+    element: <Html/>,
+  },
+  {
+    path: "css",
+    element: <Css/>,
+  },
+  {
+    path: "javascript",
+    element: <Javascript/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<RouterProvider router={router} />
   </React.StrictMode>
 );
 
